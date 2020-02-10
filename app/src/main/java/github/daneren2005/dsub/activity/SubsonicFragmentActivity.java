64590ec4f20a0807b2a263103d5d2146db4db2e2
t-m-w <tw@galac.tk>
2020-02-09 20:57:33 -0800

    Default settings should be re-considered on every settings load.

--- app/src/main/java/github/daneren2005/dsub/activity/SubsonicFragmentActivity.java
+++ app/src/main/java/github/daneren2005/dsub/activity/SubsonicFragmentActivity.java
@@ -703,11 +703,11 @@ public class SubsonicFragmentActivity extends SubsonicActivity implements Downlo
 		sessionInitialized = true;
 	}
 	private void loadSettings() {
-		PreferenceManager.setDefaultValues(this, R.xml.settings_appearance, false);
-		PreferenceManager.setDefaultValues(this, R.xml.settings_cache, false);
-		PreferenceManager.setDefaultValues(this, R.xml.settings_drawer, false);
-		PreferenceManager.setDefaultValues(this, R.xml.settings_sync, false);
-		PreferenceManager.setDefaultValues(this, R.xml.settings_playback, false);
+		PreferenceManager.setDefaultValues(this, R.xml.settings_appearance, true);
+		PreferenceManager.setDefaultValues(this, R.xml.settings_cache, true);
+		PreferenceManager.setDefaultValues(this, R.xml.settings_drawer, true);
+		PreferenceManager.setDefaultValues(this, R.xml.settings_sync, true);
+		PreferenceManager.setDefaultValues(this, R.xml.settings_playback, true);
 
 		SharedPreferences prefs = Util.getPreferences(this);
 		if (!prefs.contains(Constants.PREFERENCES_KEY_CACHE_LOCATION) || prefs.getString(Constants.PREFERENCES_KEY_CACHE_LOCATION, null) == null) {
